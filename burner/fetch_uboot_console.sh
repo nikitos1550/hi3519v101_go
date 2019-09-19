9b8d20b1d79dc50b75ad1fb34d58cc1c3472a3ba
wsnk <wsnk00@yandex.com>
2019-09-19 12:02:47 +0000

    Update script

--- burner/fetch_uboot_console.sh
+++ burner/fetch_uboot_console.sh
@@ -2,24 +2,25 @@
 
 BURNER_DIR=$(realpath $(dirname $0)/..)
 DEVICE_PY=$BURNER_DIR/device.py
+UBOOT_CONSOLE_PY=$BURNER_DIR/uboot_console.py
 POWER_PORT=/dev/ttyACM0
 POWER_BR=115200
 
-if [ $# != 2 ]; then
+if [ $# != 1 ]; then
     echo -e \
-"Fetch UBoot console on a given camera
-Usage:    $0 <camera-id> <camera-port>"
+"Reset power and fetch UBoot console on a given camera
+Usage:    $0 <camera-id>"
     exit 1
 fi
 
 CAM_ID=$1
-CAM_PORT=$2
+CAM_PORT=/dev/ttyCAM$CAM_ID
 
 # reset power
 $DEVICE_PY --port $POWER_PORT --br $POWER_BR write --wait-for "READY" --data "reset $CAM_ID"
 
 # stop kernel loading
-$DEVICE_PY --port $CAM_PORT write --wait-for "System startup" --data "A"
+$DEVICE_PY --port $CAM_PORT write --wait-for "System startup" --data "X"
 
 # open terminal =)
 $DEVICE_PY --port $CAM_PORT
