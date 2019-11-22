8a991e7f4ce8c1a6becaae9affd3ae775d07c97a
Nikita O <nikitos1550@ya.ru>
2019-11-22 17:41:46 +0300

    setup parameters to ko modules load options

--- application/pkg/koloader/koloader.go
+++ application/pkg/koloader/koloader.go
@@ -18,54 +18,53 @@ var (
 )
 
 func LoadMinimal() {
-    loadKo()
+    //loadKo(Modules[][0])
+    LoadAll() //TODO
 }
 
-/*
-func prepareOptions(map) (modules[][2], error) {
-    
-
+func LoadAll() {
+    tmpModules := make([][2]string, len(Modules))
+    copy(tmpModules, Modules[:])
+    loadKo(Modules[:])
 }
-*/
 
+func loadKo(modules [][2]string) {
+    //log.Println("Embedded files: ", AssetNames())
 
-func loadKo() {
-    log.Println("Embedded files: ", AssetNames())
+    setupKoParams(modules[:])
 
-    //setupKoParams()
-
-    for i := len(Modules)-1; i>=0; i-- {
-        err := unix.DeleteModule(Modules[i][0], 0)
+    for i := len(modules)-1; i>=0; i-- {
+        err := unix.DeleteModule(modules[i][0], 0)
         if err != nil {
-            log.Println("Rmmod ", Modules[i][0], " error ", err)
+            log.Println("Rmmod ", modules[i][0], " error ", err)
         }
     }
 
-    for i := 0; i<len(Modules); i++ {
-        data, err := Asset(Modules[i][0])
+    for i := 0; i<len(modules); i++ {
+        data, err := Asset(modules[i][0])
         if err != nil {
-            log.Println(Modules[i][0], " not found!")
+            log.Println(modules[i][0], " not found!")
             continue
         }
   
-        err2 := unix.InitModule(data, Modules[i][1])
+        err2 := unix.InitModule(data, modules[i][1])
         if err2 != nil {
-            log.Println(Modules[i][0], " error (", err2, ") loading!")
+            log.Println(modules[i][0], " error (", err2, ") loading!")
             return
         }
     }
 }
 
 
-func setupKoParams() {
+func setupKoParams(modules [][2]string) {
     var memStartAddr uint64 = 0x80000000 + (uint64(memLinux)*1024*1024)
     var memMppSize uint64 = uint64(memTotal - memLinux)
 
-    for i:=0; i<len(Modules); i++ {
+    for i:=0; i<len(modules); i++ {
         Modules[i][1] = strings.Replace(Modules[i][1], "{memStartAddr}",    strconv.FormatUint(memStartAddr, 16),       -1)
         Modules[i][1] = strings.Replace(Modules[i][1], "{memMppSize}",      strconv.FormatUint(memMppSize, 10),         -1)
         Modules[i][1] = strings.Replace(Modules[i][1], "{memTotalSize}",    strconv.FormatUint(uint64(memTotal), 10),   -1)
         Modules[i][1] = strings.Replace(Modules[i][1], "{chipName}",        chip,                                       -1)
-        log.Println(Modules[i][1])
+        log.Println(modules[i][1])
     }
 }
