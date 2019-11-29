42ec5085ba405c2fd142523b67b95a979798cda9
Nikita O <nikitos1550@ya.ru>
2019-11-29 12:07:35 +0300

    scripts initial

--- application/go.mod
+++ application/go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/gorilla/mux v1.7.3
 	github.com/shuLhan/go-bindata v3.4.0+incompatible // indirect
+	github.com/yuin/gopher-lua v0.0.0-20191128022950-c6266f4fe8d7
 	golang.org/x/sys v0.0.0-20190215142949-d0b11bdaac8a
 	golang.org/x/tools v0.0.0-20191118222007-07fc4c7f2b98 // indirect
 )
---
+++ application/pkg/scripts/scripts.go
@@ -0,0 +1,58 @@
+// +build scripts
+
+package scripts
+
+import (
+	"github.com/yuin/gopher-lua"
+    //"flag"
+)
+
+var L *lua.LState
+
+//TODO
+//func AddFunc(fname string, fimpl func(L *lua.LState) ) {
+func AddFunc(fname string, fimpl lua.LGFunction) {
+	L.SetGlobal(fname, L.NewFunction(fimpl)) /* Original lua_setglobal uses stack... */
+}
+
+func Init() {
+    //    fmt.Println("Hello from openapi Init")
+    //test = flag.Int("lua1", 8888, "path to default scripts")
+    //test = flag.Int("lua2", 8888, "path to user scripts")
+
+
+	//L = lua.NewState()
+	/*
+	L = lua.NewState(lua.Options{
+		//RegistrySize: 1024 * 20,         // this is the initial size of the registry
+		//RegistryMaxSize: 1024 * 80,      // this is the maximum size that the registry can grow to. If set to `0` (the default) then the registry will not auto grow
+		//RegistryGrowStep: 32,            // this is how much to step up the registry by each time it runs out of space. The default is `32`. 
+		//CallStackSize: 120,              // this is the maximum callstack size of this LState
+		//MinimizeStackMemory: true,       // Defaults to `false` if not specified. If set, the callstack will auto grow and shrink as needed up to a max of `CallStackSize`. If not set, the callstack will be fixed at `CallStackSize`.
+		//SkipOpenLibs: true,				 // 
+		//IncludeGoStackTrace: false,		 //
+	})
+	*/
+	L := lua.NewState(lua.Options{SkipOpenLibs: true})
+    defer L.Close()
+    for _, pair := range []struct {
+        n string
+        f lua.LGFunction
+    }{
+        {lua.LoadLibName, lua.OpenPackage}, // Must be first
+        {lua.BaseLibName, lua.OpenBase},
+        {lua.TabLibName, lua.OpenTable},
+    } {
+        if err := L.CallByParam(lua.P{
+            Fn:      L.NewFunction(pair.f),
+            NRet:    0,
+            Protect: true,
+        }, lua.LString(pair.n)); err != nil {
+            panic(err)
+        }
+    }
+	if err := L.DoString(`print("hello from LUA\n")`); err != nil {
+		panic(err)
+	}
+	//L.Close()
+}
