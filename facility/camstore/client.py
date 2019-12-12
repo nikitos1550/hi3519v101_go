0952d0346db7b61177b70871ab4d2a14bedcc837
wsnk <wsnk00@yandex.com>
2019-12-12 21:45:40 +0300

    Refactoring camstore client

--- facility/camstore/client.py
+++ facility/camstore/client.py
@@ -9,44 +9,74 @@ from lib import common
 
 
 logging.basicConfig(level=logging.WARNING)
+ALLOWED_EXEC_COMMANDS = ("telnet")
 
 
-async def request(command, port):
-    try:
+class FailedRequest(Exception):
+    def __init__(self, response):
+        self.response = response
+
+
+class Connection:
+    @classmethod
+    async def connect(cls, port):
         reader, writer = await asyncio.open_connection(host="localhost", port=port)
         await reader.readuntil(b"#")
-    except Exception as err:
-        logging.error("Could not connect to localhost:{}".format(port))
-        return
+        return cls(reader, writer)
 
-    writer.write("set_user {}".format(os.getlogin()).encode("ascii") + b"\n")
-    await reader.readuntil(b"#")
+    def __enter__(self):
+        return self
 
-    req = " ".join(command).encode("ascii") + b"\n"
-    writer.write(req)
-    try:
-        resp = (await reader.readuntil(b"#"))[:-2]
-    except asyncio.streams.IncompleteReadError as err:
-        logging.warning("Request '{}' seems failed".format(req))
-        resp = err.partial
+    def __exit__(self, *arg, **kwargs):
+        self.close()
 
-    writer.close()
-    return json.loads(resp.decode("ascii"))
+    def __init__(self, reader, writer):
+        self.reader = reader
+        self.writer = writer
+
+    async def _make_request(self, request):
+        self.writer.write(request.encode("ascii") + b"\n")
+        response = await self.reader.readuntil(b"#")
+        return response[:-2].decode("ascii")
+
+    async def request(self, req):
+        resp = json.loads(await self._make_request(req))
+        if resp["status"] != common.STATUS_OK:
+            raise FailedRequest(resp)
+        return resp
+    
+    def close(self):
+        self.writer.close()
+
+# -------------------------------------------------------------------------------------------------
+
+async def request(command, port):
+    conn = await Connection.connect(port)
+    with conn:
+        await conn.request("set_user {}".format(os.getlogin()))
+        return await conn.request(" ".join(command))
 
 
 def handle_response(resp, execute=True):
     common.print_response(resp)
     if resp.get("status") != common.STATUS_OK:
         return -1
+    
+    if not resp.get("exec"):
+        return 0
 
-    if resp["exec"]:
-        if not execute:
-            print(resp["exec"])
-        else:
-            args = resp["exec"].split(" ")
-            if args[0] == "telnet":
-                subprocess.run(args, shell=False)
-                common.print_success("Telnet session completed\n")
+    args = resp["exec"].split(" ")
+
+    if args[0] not in ALLOWED_EXEC_COMMANDS:
+        common.print_failure(f"Command '{args[0]}' is not allowed")
+        return -1
+
+    if not execute:
+        print(resp["exec"])
+        return 0
+
+    subprocess.run(args, shell=False)
+    common.print_success(f"{args[0]} session completed\n")
 
     return 0
 
@@ -63,12 +93,16 @@ def main():
     args = parser.parse_args()
 
     eloop = asyncio.get_event_loop()
-    response = eloop.run_until_complete(request(command=args.command, port=args.port))
-    if response is None:
+    try:
+        response = eloop.run_until_complete(request(command=args.command, port=args.port))
+        rc = handle_response(response, execute=(not args.no_exec))
+        exit(rc)
+    except FailedRequest as err:
+        common.print_response(err.response)
         exit(-1)
-
-    rc = handle_response(response, execute=(not args.no_exec))
-    exit(rc)
+    except Exception as err:
+        logging.error(err)
+        exit(-2)
 
 
 main()
