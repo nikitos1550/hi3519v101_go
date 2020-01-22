f41e51ed96561e7f5780b0d731760edfaf66a91c
Nikita O <nikitos1550@ya.ru>
2020-01-22 12:15:25 +0300

    initial for registers util

---
+++ application/pkg/utils/regs/README.md
@@ -0,0 +1,6 @@
+# hisilicon_rdl
+HiSilicon CCTV chips RDL2.0 register description
+
+## notes
+* https://github.com/Juniper/open-register-design-tool
+* https://pypi.org/project/systemrdl-compiler/
---
+++ application/pkg/utils/regs/test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+java -jar ./Ordt.jar -parms ./test.params -rdl 1.rdl -xml 1.xml -reglist 1.reglist -pydrvmod 1.py -uvmregs 1.uvmregs -uvmregspkg 1.uvmregspkg -cppmod 1.cppmod -cppdrvmod 1.cppdrvmod ./test.rdl
+
