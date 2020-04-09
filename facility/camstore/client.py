00854a93ea00a4204893b6242f474ec26f9f952a
wsnk <wsnk00@yandex.com>
2020-04-09 16:17:17 +0300

    [camstore] Add client's option to run quite

--- facility/camstore/client.py
+++ facility/camstore/client.py
@@ -63,8 +63,9 @@ async def request(command, port):
         return await conn.request(" ".join(command))
 
 
-def handle_response(resp, execute=True):
-    common.print_response(resp)
+def handle_response(resp, execute=True, quite=False):
+    if not quite:
+        common.print_response(resp)
     if resp.get("status") != common.STATUS_OK:
         return -1
     
@@ -74,7 +75,8 @@ def handle_response(resp, execute=True):
     args = resp["exec"].split(" ")
 
     if args[0] not in ALLOWED_EXEC_COMMANDS:
-        common.print_failure(f"Command '{args[0]}' is not allowed")
+        if not quite:
+            common.print_failure(f"Command '{args[0]}' is not allowed")
         return -1
 
     if not execute:
@@ -82,7 +84,8 @@ def handle_response(resp, execute=True):
         return 0
 
     subprocess.run(args, shell=False)
-    common.print_success(f"{args[0]} session completed\n")
+    if not quite:
+        common.print_success(f"{args[0]} session completed\n")
 
     return 0
 
@@ -94,6 +97,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", "-p", type=int, default=43500, help="TCP port of the daemon")
     parser.add_argument("--no-exec", action="store_true", help="Do not execute (telnet)")
+    parser.add_argument("--quite", action="store_true", help="Do not print info, just do")
+
     parser.add_argument("command", nargs="+", type=str, help="Command and arguments")
 
     args = parser.parse_args()
@@ -103,7 +108,7 @@ def main():
     eloop = asyncio.get_event_loop()
     try:
         response = eloop.run_until_complete(request(command=args.command, port=args.port))
-        rc = handle_response(response, execute=(not args.no_exec))
+        rc = handle_response(response, execute=(not args.no_exec), quite=args.quite)
         exit(rc)
     except FailedRequest as err:
         common.print_response(err.response)
