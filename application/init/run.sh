779099a108bdd799e4c55bbb1048b9ca07f3a5bc
Nikita O <nikitos1550@ya.ru>
2020-04-10 13:10:23 +0300

    init scripts updated

--- application/init/run.sh
+++ application/init/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 . /opt/board.config
-#dmesg -n 1
-#/opt/application
+dmesg -n 1
+
+/opt/gohisiprobe -mem-total=$RAM_SIZE -mem-linux=$RAM_LINUX_SIZE -mem-mpp=$RAM_MPP_SIZE &
 /opt/gohisicam -mem-total=$RAM_SIZE -mem-linux=$RAM_LINUX_SIZE -mem-mpp=$RAM_MPP_SIZE &
 
