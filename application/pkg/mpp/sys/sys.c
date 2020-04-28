206a00d0877a9bbe82aa0ee72aece0e950f4bf6c
Nikita O <nikitos1550@ya.ru>
2020-04-28 22:33:49 +0300

    sys finally combined

--- application/pkg/mpp/sys/sys.c
+++ application/pkg/mpp/sys/sys.c
@@ -1,6 +1,5 @@
 #include "sys.h"
 
-#if defined HI3516AV100 || defined HI3516AV200 || defined HI3516CV100 || defined HI3516CV200 || defined HI3516CV300 
 int mpp_sys_init(error_in *err, mpp_sys_init_in *in) {
     unsigned int mpp_error_code = 0;
     
@@ -14,6 +13,8 @@ int mpp_sys_init(error_in *err, mpp_sys_init_in *in) {
         RETURN_ERR_MPP(ERR_F_HI_MPI_VB_Exit, mpp_error_code);
     }
 
+    #if defined(HI3516AV100) || defined(HI3516AV200) || defined(HI3516CV100) || defined(HI3516CV200) || defined(HI3516CV300)
+
     VB_CONF_S stVbConf;
 
     memset(&stVbConf, 0, sizeof(VB_CONF_S));
@@ -26,7 +27,30 @@ int mpp_sys_init(error_in *err, mpp_sys_init_in *in) {
     if (mpp_error_code != HI_SUCCESS) {
         RETURN_ERR_MPP(ERR_F_HI_MPI_VB_SetConf, mpp_error_code);
     }
-    
+
+    #endif // defined(HI3516AV100) || defined(HI3516AV200) || defined(HI3516CV100) || defined(HI3516CV200) || defined(HI3516CV300)
+  
+    #if defined(HI3516CV500) || defined(HI3516EV200) || defined(HI3519AV100) || defined(HI3559AV100)
+
+    VB_CONFIG_S        stVbConf;
+
+    memset(&stVbConf,0,sizeof(VB_CONFIG_S));
+    stVbConf.u32MaxPoolCnt              = 2;
+    stVbConf.astCommPool[0].u64BlkSize = COMMON_GetPicBufferSize(   in->width, 
+                                                                    in->height, 
+                                                                    PIXEL_FORMAT_YVU_SEMIPLANAR_420, 
+                                                                    DATA_BITWIDTH_8, 
+                                                                    COMPRESS_MODE_SEG, 
+                                                                    DEFAULT_ALIGN);
+    stVbConf.astCommPool[0].u32BlkCnt = in->cnt;
+
+    mpp_error_code = HI_MPI_VB_SetConfig(&stVbConf);
+    if (mpp_error_code != HI_SUCCESS) {
+        RETURN_ERR_MPP(ERR_F_, mpp_error_code);
+    }
+
+    #endif // defined(HI3516CV500) || defined(HI3516EV200) || defined(HI3519AV100) || defined(HI3559AV100)    
+
     mpp_error_code = HI_MPI_VB_Init();
     if (mpp_error_code != HI_SUCCESS) {
         RETURN_ERR_MPP(ERR_F_HI_MPI_VB_Init, mpp_error_code);
@@ -49,60 +73,3 @@ int mpp_sys_init(error_in *err, mpp_sys_init_in *in) {
 
     return ERR_NONE;
 }
-#endif // defined HI3516AV100 || defined HI3516AV200 || defined HI3516CV100 || defined HI3516CV200 || defined HI3516CV300
-
-#if defined HI3516CV500 || defined HI3516EV200 || defined HI3519AV100 || defined HI3559AV100
-int mpp_sys_init(error_in *err, mpp_sys_init_in *in) {
-    unsigned int mpp_error_code = 0;
-
-    mpp_error_code = HI_MPI_SYS_Exit();
-    if (mpp_error_code != HI_SUCCESS) {
-        RETURN_ERR_MPP(ERR_F_, mpp_error_code);
-    }
-
-    mpp_error_code = HI_MPI_VB_Exit();
-    if (mpp_error_code != HI_SUCCESS) {
-        RETURN_ERR_MPP(ERR_F_, mpp_error_code);
-    }
-
-
-    VB_CONFIG_S        stVbConf;
-
-    memset(&stVbConf,0,sizeof(VB_CONFIG_S));
-    stVbConf.u32MaxPoolCnt              = 2;
-    stVbConf.astCommPool[0].u64BlkSize = COMMON_GetPicBufferSize(   in->width, 
-                                                                    in->height, 
-                                                                    PIXEL_FORMAT_YVU_SEMIPLANAR_420, 
-                                                                    DATA_BITWIDTH_8, 
-                                                                    COMPRESS_MODE_SEG, 
-                                                                    DEFAULT_ALIGN);
-    stVbConf.astCommPool[0].u32BlkCnt = in->cnt;
-
-    mpp_error_code = HI_MPI_VB_SetConfig(&stVbConf);
-    if (mpp_error_code != HI_SUCCESS) {
-        RETURN_ERR_MPP(ERR_F_, mpp_error_code);
-    }
-
-    mpp_error_code = HI_MPI_VB_Init();
-    if (mpp_error_code != HI_SUCCESS) {
-        RETURN_ERR_MPP(ERR_F_, mpp_error_code);
-    }
-
-    MPP_SYS_CONF_S stSysConf;
-
-    memset(&stSysConf, 0, sizeof(MPP_SYS_CONF_S));
-    stSysConf.u32AlignWidth = 64;
-
-    mpp_error_code = HI_MPI_SYS_SetConf(&stSysConf);
-    if (mpp_error_code != HI_SUCCESS) {
-        RETURN_ERR_MPP(ERR_F_, mpp_error_code);
-    }
-
-    mpp_error_code = HI_MPI_SYS_Init();
-    if (mpp_error_code != HI_SUCCESS) {
-        RETURN_ERR_MPP(ERR_F_, mpp_error_code);
-    }
-
-    return ERR_NONE;
-}
-#endif // defined HI3516CV500 || defined HI3516EV200 || defined HI3519AV100 || defined HI3559AV100
