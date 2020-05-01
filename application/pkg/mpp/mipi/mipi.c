b8f87fb0a752334675c5f6f34accf0f14afd8069
Nikita O <nikitos1550@ya.ru>
2020-05-01 18:30:05 +0300

    mipi optimized

--- application/pkg/mpp/mipi/mipi.c
+++ application/pkg/mpp/mipi/mipi.c
@@ -15,23 +15,10 @@ int mpp_mipi_init(error_in *err, mpp_mipi_init_in *in) {
     }
 
     combo_dev_attr_t stcomboDevAttr;
+
     memcpy(&stcomboDevAttr, in->mipi, sizeof(combo_dev_attr_t));
 
-    #if HI_MPP == 3
-        stcomboDevAttr.devno = 0; //TODO
-
-        general_error_code = ioctl(fd, HI_MIPI_RESET_MIPI, &stcomboDevAttr.devno);
-        if (general_error_code != 0){
-            close(fd);
-            RETURN_ERR_GENERAL(err, "HI_MIPI_RESET_MIPI", general_error_code);
-        }
-
-        general_error_code = ioctl(fd, HI_MIPI_RESET_SENSOR, &stcomboDevAttr.devno); 
-        if (general_error_code != 0) {
-            close(fd);
-            RETURN_ERR_GENERAL(err, "HI_MIPI_RESET_SENSOR", general_error_code);
-        }
-    #endif
+    stcomboDevAttr.devno = 0; //TODO
 
     #if HI_MPP == 4
     	lane_divide_mode_t lane_divide_mode = LANE_DIVIDE_MODE_0;
@@ -42,29 +29,27 @@ int mpp_mipi_init(error_in *err, mpp_mipi_init_in *in) {
             RETURN_ERR_GENERAL(err, "HI_MIPI_SET_HS_MODE", general_error_code);
         }
 
-        combo_dev_t devno = 0;
-
-        general_error_code = ioctl(fd, HI_MIPI_ENABLE_MIPI_CLOCK, &devno);
+        general_error_code = ioctl(fd, HI_MIPI_ENABLE_MIPI_CLOCK, &stcomboDevAttr.devno);   //&devno);
         if (general_error_code != HI_SUCCESS) {
             close(fd);
             RETURN_ERR_GENERAL(err, "HI_MIPI_ENABLE_MIPI_CLOCK", general_error_code);
         }
 
-        general_error_code = ioctl(fd, HI_MIPI_RESET_MIPI, &devno);
+        general_error_code = ioctl(fd, HI_MIPI_ENABLE_SENSOR_CLOCK, &stcomboDevAttr.devno); //&SnsDev);
+        if (general_error_code != HI_SUCCESS) {
+            close(fd);
+            RETURN_ERR_GENERAL(err, "HI_MIPI_ENABLE_SENSOR_CLOCK", general_error_code); 
+        }
+    #endif
+
+    #if HI_MPP >= 3
+        general_error_code = ioctl(fd, HI_MIPI_RESET_MIPI, &stcomboDevAttr.devno);  //&devno);
         if (general_error_code != HI_SUCCESS) {
             close(fd);
             RETURN_ERR_GENERAL(err, "HI_MIPI_RESET_MIPI", general_error_code); 
         }
 
-        sns_clk_source_t SnsDev = 0;
-
-        general_error_code = ioctl(fd, HI_MIPI_ENABLE_SENSOR_CLOCK, &SnsDev);
-        if (general_error_code != HI_SUCCESS) {
-            close(fd);
-            RETURN_ERR_GENERAL(err, "HI_MIPI_ENABLE_SENSOR_CLOCK", general_error_code); 
-       	}
-
-        general_error_code = ioctl(fd, HI_MIPI_RESET_SENSOR, &SnsDev);
+        general_error_code = ioctl(fd, HI_MIPI_RESET_SENSOR, &stcomboDevAttr.devno);    //&SnsDev);
         if (general_error_code != HI_SUCCESS) {
             close(fd);
             RETURN_ERR_GENERAL(err, "HI_MIPI_RESET_SENSOR", general_error_code); 
@@ -77,7 +62,7 @@ int mpp_mipi_init(error_in *err, mpp_mipi_init_in *in) {
         RETURN_ERR_GENERAL(err, "HI_MIPI_SET_DEV_ATTR", general_error_code); 
     }
 
-    #if HI_MPP == 3
+    #if HI_MPP >= 3
    		#if defined(HI3516CV300)
        		usleep(10000);
         #endif
@@ -93,20 +78,6 @@ int mpp_mipi_init(error_in *err, mpp_mipi_init_in *in) {
             close(fd);
             RETURN_ERR_GENERAL(err, "HI_MIPI_UNRESET_SENSOR", general_error_code); 
         }
-	#endif
-
-    #if HI_MPP == 4
-    	general_error_code = ioctl(fd, HI_MIPI_UNRESET_MIPI, &devno);
-        if (general_error_code != HI_SUCCESS) {
-            close(fd);
-            RETURN_ERR_GENERAL(err, "HI_MIPI_UNRESET_MIPI", general_error_code); 
-        }
-
-        general_error_code = ioctl(fd, HI_MIPI_UNRESET_SENSOR, &SnsDev);
-        if (general_error_code != HI_SUCCESS) {
-            close(fd);
-            RETURN_ERR_GENERAL(err, "HI_MIPI_UNRESET_SENSOR", general_error_code); 
-     	}
 	#endif
 
     close(fd);
