fc4f337e15d57915585c7811672054bf3f69dda6
Dmitry Kirsanov <dkirsanov@aligntech.com>
2020-06-01 23:03:29 +0300

    yuv processing

--- application/pkg/mpp/venc/encoder.go
+++ application/pkg/mpp/venc/encoder.go
@@ -118,13 +118,10 @@ func (encoder ActiveEncoder) GetId() int {
 
 func (encoder ActiveEncoder) DataCallback(data []byte) {
 	for ch,_ := range encoder.DataChannels {
-log.Println("loop")
 		if (cap(ch) <= len(ch)) {
-log.Println("pop")
 			<-ch
 		}
 
-log.Println("send")
 		ch <- data
 	}
 }
