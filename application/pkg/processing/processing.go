a7b599ec6581324ce82c6e3da70edefa37b731fe
Nikita O <nikitos1550@ya.ru>
2020-06-24 15:02:40 +0300

    facility upd

--- application/pkg/processing/processing.go
+++ application/pkg/processing/processing.go
@@ -5,6 +5,7 @@ package processing
 /*
 #include "processing.h"
 
+//TODO move to venc package!!!
 int sendToEncoder(error_in *err, unsigned int vencId, void* frame) {
     #if HI_MPP == 1
         DO_OR_RETURN_ERR_MPP(err, HI_MPI_VENC_SendFrame, vencId, frame);
--- application/pkg/processing/processing.h
+++ application/pkg/processing/processing.h
@@ -3,5 +3,6 @@
 #include "../mpp/include/mpp.h"
 #include "../mpp/errmpp/errmpp.h"
 
+//TODO move to venc package!!!
 int sendToEncoder(error_in *err, unsigned int vencId, void* frame);
 
