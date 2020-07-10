47fa8f6a23ccbb6bce3715e4f5b9da6035130b43
Nikita O <nikitos1550@ya.ru>
2020-07-10 13:38:16 +0300

    missed file

---
+++ application/cmd/gohisicam/openapi.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    _ "application/pkg/openapi/encoder"
+
+    _ "application/pkg/openapi/channel"
+    _ "application/pkg/openapi/encoder"
+
+    _ "application/pkg/openapi/jpeg"
+    //_ "application/pkg/openapi/mjpeg"
+    //_ "application/pkg/openapi/webrtc"
+)
+
